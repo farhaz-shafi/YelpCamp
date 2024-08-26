dcbf6655336f5ac784ce016b0752ac3a8a00a7f2
farhaz-shafi <farhazshafi@gmail.com>
2024-08-26 13:08:02 +0530

    Initial commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 
 mongoose.connect(dbUrl);
 
