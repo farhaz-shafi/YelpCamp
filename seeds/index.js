146f0aa685a406dd03d9a096ae36906c12082a98
farhaz-shafi <farhazshafi@gmail.com>
2024-08-28 14:37:36 +0530

    added final update

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: '66b1c9dc020554df4fcc5c43',
+      // author: '66b1c9dc020554df4fcc5c43',author: '66cee7e81820f33a51198804',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: `https://picsum.photos/400?random=${Math.random()}`,
